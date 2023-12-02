3606d9a271b5b7caf19e82e91640d44f5a657167
stand4rd <thomas.noble.jordan@gmail.com>
2023-12-02 11:32:43 -0800

    fixed front end and backend

--- SmallSIEM/app.py
+++ SmallSIEM/app.py
@@ -1,58 +1,33 @@
-from flask import Flask, render_template, request
-import mysql.connector
+from flask import Flask, render_template, request, jsonify
+from query import *
+import json
 
-app = Flask(__name__, template_folder='Frontend')
-
-
-# Function to connect to MySQL database
-def connect_to_db():
-    return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="CPSC408!",
-        database="SIEM_DB"
-    )
-
-# Function to execute SQL queries
-def execute_query(query, fetch_all=False):
-    db = None  # Initialize db
-    try:
-        db = connect_to_db()
-        cursor = db.cursor()
-        cursor.execute(query)
-        
-        if fetch_all:
-            result = cursor.fetchall()
-        else:
-            result = cursor.fetchone()
-
-        return result
-
-    except Exception as e:
-        if db and db.is_connected():  # Check if db is not None before using it
-            db.rollback()
-        print(f"Error: {str(e)}")
-        return None
-
-    finally:
-        if db and db.is_connected():  # Check if db is not None before using it
-            cursor.close()
-            db.close()
-
+app = Flask(__name__, template_folder='templates')
 
 # Route for the home page
 @app.route('/')
 def index():
     content = "Hello, World!"
-    return render_template('index.html', content=content)
+    return content
 
-# Example route for displaying records
+# Route to render the form to enter the table name
+@app.route('/display_records', methods=['GET'])
+def display_records_form():
+    return render_template('display_records.html')
+
+# Route to handle form submission and display records based on the provided table name
 @app.route('/display_records', methods=['POST'])
-def display_records():
-    table_name = request.form['table_name']
-    query = f"SELECT * FROM {table_name};"
-    records = execute_query(query, fetch_all=True)
-    return render_template('display_records.html', table_name=table_name, records=records)
+def display_records_route():
+    try:
+        table_name = request.form['table']  # Get the table name from the submitted form
+
+        # Call the function from query.py to display records for the specified table
+        result = display_records(table_name)
+
+        # Return the result as JSON or in any other suitable format
+        return jsonify(result)
+    except Exception as e:
+        return jsonify({'error': str(e)})  # Return error message if an exception occurs
 
 # You need to add similar routes for other functionalities (e.g., query_with_filters, create_new_record, delete_records, etc.)
 
